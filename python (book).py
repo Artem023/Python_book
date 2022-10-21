9e9d41f170e778d600c03e5326c007439b95bf95
Artem Neliubov <artemneliubov@yandex.ru>
2022-10-21 09:51:40 +0300

    017 DONE

--- python (book).py
+++ python (book).py
@@ -32,6 +32,16 @@
 
 #017
 
+age = int (input ('Введите свой возраст: '))
+if age >= 18:
+    print ('You can vote!')
+elif age == 17:
+    print ('You can learn to drive!')
+elif age == 16:
+    print ('You can buy a lottery ticket!')
+else:
+    print ('You can go Trick-or-Treating') 
+
 #018
 
 #019
