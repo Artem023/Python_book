05edebb4d7a00de076dc8c06900338d1d8a36bba
Artem Neliubov <artemneliubov@yandex.ru>
2022-10-21 10:33:59 +0300

    019 DONE

--- python (book).py
+++ python (book).py
@@ -36,6 +36,16 @@
 
 #019
 
+num = int (input ('Введите число 1, 2 или 3: '))
+if num == 1:
+    print ('Thank you')
+elif num == 2:
+    print ('Well done')
+elif num == 3:
+    print ('Correct')
+else:
+    print ('Error messege')
+
 #020
 
 #021
